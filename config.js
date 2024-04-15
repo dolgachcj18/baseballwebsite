d781677a60106b3f5b214057b8f5b853b1f604e9
DolgachCJ18 <dolgachcj18@uww.edu>
2024-04-15 14:14:06 -0500

    test

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
-// AWS Cognito Configuration
-const poolData = {
-    UserPoolId: 'us-east-2_3UU1rk0IH', // Replace 'us-east-1_XXXXXXX' with your actual Cognito User Pool ID
-    ClientId: '4gps9mrt82harj0pmkl4fjqcm2', // Replace 'XXXXXXXXXXXXXXXXXXXXXXXXXX' with your actual Cognito App Client ID
-    region: 'us-east-2' // Replace 'us-east-1' with the AWS region your Cognito user pool is hosted in
+
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_3UU1rk0IH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gps9mrt82harj0pmkl4fjqcm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
 };
-
-// This exports the configuration data so it can be used in other JavaScript files
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports.poolData = poolData;
-} else {
-    window.poolData = poolData; // Make it available in the browser window object
-}
-
-
