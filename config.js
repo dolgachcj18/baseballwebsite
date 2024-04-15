eeddc9bdf15b2b7c1427f838e849de4eea2ae690
DolgachCJ18 <dolgachcj18@uww.edu>
2024-04-15 14:58:55 -0500

    added config back

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_3UU1rk0IH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gps9mrt82harj0pmkl4fjqcm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
