0d1d8b2dd5993ddeb106b2f7479edc5f9652c017
DolgachCJ18 <dolgachcj18@uww.edu>
2024-04-15 14:36:25 -0500

    fixing the javascript config

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-2_3UU1rk0IH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4gps9mrt82harj0pmkl4fjqcm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+<script>
+window.onload = function() {
+    // Define the poolData for Amazon Cognito
+    var poolData = {
+        UserPoolId: 'us-east-2_3UU1rk0IH',
+        ClientId: '4gps9mrt82harj0pmkl4fjqcm2'
+    };
+
+    // Get the modal
+    var modal = document.getElementById('loginModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the page loads, open the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
