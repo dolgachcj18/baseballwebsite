8ebe575471a795fb9b061f8d31f823f87740bf78
dolgachcj18 <dolgachcj18@uww.edu>
2024-04-17 10:59:41 -0500

    update

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-2_3UU1rk0IH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4gps9mrt82harj0pmkl4fjqcm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+// Example format in config.js
+var poolData = {
+    UserPoolId : 'us-east-2_3UU1rk0IH', // Your User Pool ID
+    ClientId : '4gps9mrt82harj0pmkl4fjqcm2' // Your Client ID
 };
